fd7db1d57a23cf317ffccfb23c75a1954f58c152
Masahide Ishida <masa.ishi.right@gmail.com>
2019-01-28 07:18:29 +0900

    create pylab plot practice

---
+++ c11/yearsofcompounding.py
@@ -0,0 +1,13 @@
+import pylab
+principal = 10000
+interest_rate = 0.05
+years = 20
+values = []
+for i in range(years+1):
+    values.append(principal)
+    principal += principal * interest_rate
+pylab.plot(values)
+pylab.title('5% Growth, Compounded Annually')
+pylab.xlabel('Years of Compounding')
+pylab.ylabel('Value of Principal ($)')
+pylab.savefig('years_of_compounding')
